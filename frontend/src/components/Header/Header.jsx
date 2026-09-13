14152c77834a5755ec0b948255cf552fd60b26dc
m3rc1kk <maksimkakcmblcl@gmail.com>
2026-09-13 19:37:29 +0300

    Components: Header, Button, Logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
 import ButtonLink from '../Button/Button.jsx';
 import Logo from '../Logo/Logo.jsx';
+import telegramIcon from '../../assets/images/Header/telegram.svg'
 
 export default function Header() {
     return (
-        <></>
+        <header className="header container">
+            <div className="header__inner">
+
+                <div className="header__logo">
+                    <Logo className={'logo__image'} />
+                </div>
+
+                <nav className="header__nav">
+                    <ul className="header__nav-list">
+                        <li className="header__nav-item">
+                            <ButtonLink to={'/'} className={'header__nav-link button-text'}>Главная</ButtonLink>
+                        </li>
+                        <li className="header__nav-item">
+                            <ButtonLink to={'/'} className={'header__nav-link button-text'}>О нас</ButtonLink>
+                        </li>
+                        <li className="header__nav-item">
+                            <ButtonLink to={'/'} className={'header__nav-link button-text'}>Победители</ButtonLink>
+                        </li>
+                        <li className="header__nav-item">
+                            <ButtonLink to={'/'} className={'header__nav-link button-text'}>Голосование</ButtonLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <ButtonLink className="header__login" to="/">Войти <img src={telegramIcon} width={16} height={16} loading={'lazy'} alt="Telegram" className="header__login-icon"/></ButtonLink>
+
+            </div>
+        </header>
     );
 }
--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,7 +1,9 @@
+import Header from "../../components/Header/Header.jsx";
+
 export default function Main() {
     return (
         <>
-            <div className="div">PENIS</div>
+            <Header />
         </>
     );
 }
